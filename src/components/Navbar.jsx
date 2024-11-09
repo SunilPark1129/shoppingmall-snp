238b57925b6986ca70d60d3bd065cc50e9d71213
Sunil Park <sunilpark1129@gmail.com>
2024-11-09 13:20:51 -0500

    added - navbar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,22 +3,33 @@ import "./style/navbar.css";
 import { navLayout } from "../data/navigationLinks";
 import { Link } from "react-router-dom";
 import "./style/navbar.css";
+import { SvgSearch, SvgUser, SvgTable, SvgBag } from "../svg/SVGFiles";
 
 function Navbar() {
   return (
     <header className="navbar">
       <div className="nav-top">
         <label className="input-label">
-          {/* svg */}
+          <div className="nav-search svg-box">{SvgSearch}</div>
           <input type="text" autoComplete="off" placeholder="Search Product" />
         </label>
         <div className="nav-btns">
-          <button>Login</button>
-          <button>Cart(0)</button>
-          <button>My Order</button>
+          <button>
+            <div className="svg-box">{SvgUser}</div> Login
+          </button>
+          <button>
+            <div className="svg-box">{SvgBag}</div> Cart(0)
+          </button>
+          <button>
+            <div className="svg-box">{SvgTable}</div> My Order
+          </button>
         </div>
       </div>
-      <div className="nav-logo img-box">{/* logo here */}</div>
+      <Link className="nav-logo-a" to={"/"}>
+        <div className="nav-logo img-box">
+          <img src="/image/sparklogo.png" alt="" />
+        </div>
+      </Link>
       <nav>
         <ul className="nav-list">
           {navLayout.map(({ to, label }) => {
---
+++ src/svg/SVGFiles.js
@@ -0,0 +1,56 @@
+export const SvgSearch = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+    <path d="M21 21l-6 -6" />
+  </svg>
+);
+
+export const SvgUser = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path d="M12 2a5 5 0 1 0 5 5 5 5 0 0 0-5-5zm0 8a3 3 0 1 1 3-3 3 3 0 0 1-3 3zm9 11v-1a7 7 0 0 0-7-7h-4a7 7 0 0 0-7 7v1h2v-1a5 5 0 0 1 5-5h4a5 5 0 0 1 5 5v1z"></path>
+  </svg>
+);
+
+export const SvgTable = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M4 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
+    <path d="M14 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
+    <path d="M4 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
+    <path d="M14 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
+  </svg>
+);
+
+export const SvgBag = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path d="M5 22h14a2 2 0 0 0 2-2V9a1 1 0 0 0-1-1h-3v-.777c0-2.609-1.903-4.945-4.5-5.198A5.005 5.005 0 0 0 7 7v1H4a1 1 0 0 0-1 1v11a2 2 0 0 0 2 2zm12-12v2h-2v-2h2zM9 7c0-1.654 1.346-3 3-3s3 1.346 3 3v1H9V7zm-2 3h2v2H7v-2z"></path>
+  </svg>
+);
