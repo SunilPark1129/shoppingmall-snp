6864cd85e9359885c5d11ee72d922f299a96c5f9
Sunil Park <sunilpark1129@gmail.com>
2024-11-09 11:19:43 -0500

    added - nav layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import "./style/navbar.css";
+import { navLayout } from "../data/navigationLinks";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <header>
+      <div className="nav-top">
+        <label className="input-label">
+          {/* svg */}
+          <input type="text" autoComplete="off" placeholder="Search Product" />
+        </label>
+        <div className="nav-btns">
+          <button>Login</button>
+          <button>Cart(0)</button>
+          <button>My Order</button>
+        </div>
+      </div>
+      <div className="nav-logo img-box">{/* logo here */}</div>
+      <nav>
+        <ul className="nav-list">
+          {navLayout.map(({ to, label }) => {
+            return (
+              <li key={label} className="nav-item">
+                <Link to={to}>{label}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
