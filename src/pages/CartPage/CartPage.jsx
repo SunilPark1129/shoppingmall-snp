2fd83ffcea623545a27cfdae45ba769d860568a5
Sunil Park <sunilpark1129@gmail.com>
2024-12-07 05:49:41 -0500

    add: style

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -2,61 +2,65 @@ import React from 'react';
 import './index.css';
 
 function CartPage() {
+  const items = true;
   return (
     <main className="cart">
       <div className="wrapper">
-        <div className="cart__container">
+        <h1>CART INFORMATION</h1>
+        <div className="cart__content">
           {/* item */}
-          <article className="cart__item">
-            {/* item === 0 */}
-            <div className="cart__empty">
-              <div>Your cart is empty</div>
-              <div>Please add Items</div>
-            </div>
-            {/* item !== 0 */}
-            <section className="cart__card">
-              <div className="image-container">
-                <img src="" alt="" />
+          <article className="cart__items">
+            {items ? (
+              <div className="cart__empty">
+                <div>Your cart is empty</div>
+                <span>Please add Items</span>
               </div>
-              <div className="cart__card-text">
-                <div className="cart__card-top">
-                  <h2>name</h2>
-                  <div>$ 0</div>
-                  <div>Size: m</div>
+            ) : (
+              <section className="cart__card">
+                <div className="image-container">
+                  <img src="" alt="" />
                 </div>
-                <div className="cart__card-bot">
-                  <div>Total: $ 0</div>
-                  <div>
-                    <div>Quantity: </div>
-                    <input type="text" />
+                <div className="cart__card-text">
+                  <div className="cart__card-top">
+                    <h2>name</h2>
+                    <div>$ 0</div>
+                    <div>Size: m</div>
+                  </div>
+                  <div className="cart__card-bot">
+                    <div>Total: $ 0</div>
+                    <div>
+                      <div className="cart__card-qt">Quantity: </div>
+                      <input type="text" />
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="cart__card-btn">
-                <button>detail</button>
-                <button>trash</button>
-              </div>
-            </section>
+                <div className="cart__card-btn">
+                  <button>detail</button>
+                  <button>trash</button>
+                </div>
+              </section>
+            )}
           </article>
 
           {/* info */}
           <article className="cart__info">
-            <h1>Cart Info</h1>
+            <h2>Order History</h2>
             <div className="cart__price">
-              <div className="cart__price-item">
-                <div className="cart__price-card">
-                  <div>name</div>
+              <div className="cart__price-items">
+                <section className="cart__price-card">
+                  <h3>name</h3>
                   <div>$ 0</div>
-                </div>
+                </section>
               </div>
-              <div className="cart__payment">
-                <button>Continue to payment</button>
-              </div>
+
+              <div className="line"></div>
+
               <div className="cart__price-total">
-                <div>Total:</div>
+                <div>TOTAL</div>
                 <div>$ 0</div>
               </div>
             </div>
+            <button className="cart__price-btn">Continue to payment</button>
             <p>
               Payment methods available: The prices and shipping fees will not
               be confirmed until you reach the payment stage. You can return
